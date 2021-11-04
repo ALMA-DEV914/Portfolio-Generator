4ea37492eedf154d0bb2e809538ba2811e1312c0
ALMA_DEV914 <aungonalna58@gamil.com>
2021-11-04 16:29:23 -0700

    Add another file to generate web page

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
 const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
 //const name =profileDataArgs[0];
 //const github = profileDataArgs[1];
  const [name, github] = profileDataArgs;
 
-const generatePage = (name, github) => {
-return `
-       <DOCTYPE html>
-       <html lang = "en">
-       <head>
-       <meta charset = "UTF-8">
-       <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-       <meta http-equiv= "X-UA-compatible" content= "ie=edge">
-       <title>Portfolio Generator</title>
-       </head>
 
-       <body>
-       <h1>${name}</h1>
-       <h2><a href= "https://github.com/${github}"> Github </a></h2>
-       </body>
-       </html>
-       `;
-};
 //console.log(name, github);
 //console.log (generatePage(name, github));
 fs.writeFile('index.html', generatePage(name, github), err => {
